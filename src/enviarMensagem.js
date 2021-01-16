6ed609d3db525aeb565b56d4be3107581a030222
herik-silva <herik.silva.019@gmail.com>
2021-01-16 19:26:38 -0300

    Palavras reservadas ordenadas em ordem alfabetica

--- src/enviarMensagem.js
+++ src/enviarMensagem.js
@@ -1,7 +1,6 @@
 const palavrasReservadas = require('./palavrasReservadas');
 
 // Envia a mensagem de acordo com a palavra reservada
-
 module.exports = function enviarMensagem(client){
     client.onMessage(async mensagem =>{
         const comando = mensagem.body;
@@ -12,12 +11,8 @@ module.exports = function enviarMensagem(client){
 
         const prefixoCorreto = palavrasReservadas.prefixo == comando.charAt(0);
         const comandoExiste = palavrasReservadas[palavraReservada] != undefined;
-
-        console.log("Resultado do teste logico: " + prefixoCorreto + " && " + comandoExiste);
         
         if(comandoExiste && prefixoCorreto){
-            console.log(palavraReservada);
-
             palavrasReservadas[palavraReservada](client,mensagem);
         }
     });
--- src/palavrasReservadas.js
+++ src/palavrasReservadas.js
@@ -1,11 +1,11 @@
 module.exports = {
     prefixo: "!",
 
-    "CRIADORES": async(client,mensagem)=>{
-        await client.sendText(mensagem.from, "Criado por: Herik Ramos & Marco Antônio");
-    },
     "AJUDA": async(client,mensagem)=>{
         await client.sendText(mensagem.from, "Olá");
+    },
+    "CRIADORES": async(client,mensagem)=>{
+        await client.sendText(mensagem.from, "Criado por: Herik Ramos & Marco Antônio");
     },
     "TUDO BEM": async(client,mensagem)=>{
         await client.sendText(mensagem.from, "Tudo e com vc?");
