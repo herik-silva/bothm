07f9bd464f0c6c206ca672e069aef0f3676833ff
MarcoAs25 <marcoas2566@gmail.com>
2021-01-16 18:14:54 -0300

    Uma nova palavra reservada

--- src/palavrasReservadas.js
+++ src/palavrasReservadas.js
@@ -4,5 +4,8 @@ module.exports = {
     },
     "Tudo Bem": async(client,mensagem)=>{
         await client.sendText(mensagem.from, "Tudo e com vc?");
+    },
+    "Criadores": async(client,mensagem)=>{
+        await client.sendText(mensagem.from, "Criado por: Herik Ramos & Marco Antônio");
     }
 }
