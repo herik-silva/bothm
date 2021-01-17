3386557ee4a0ff4cabeaf15c512e61785227dfe9
MarcoAs25 <marcoas2566@gmail.com>
2021-01-17 01:44:21 -0300

    Adicionado nova lista de comandos

--- src/enviarMensagem.js
+++ src/enviarMensagem.js
@@ -3,7 +3,6 @@ const palavrasReservadas = require('./palavrasReservadas');
 // Envia a mensagem de acordo com a palavra reservada
 module.exports = function enviarMensagem(client){
     client.onMessage(async mensagem =>{
-        const comando = mensagem.body;
         
         const palavraReservada = comando.split(palavrasReservadas.prefixo)[1].toUpperCase();
 
--- src/palavrasReservadas.js
+++ src/palavrasReservadas.js
@@ -10,12 +10,41 @@ const palavrasReservadas = {
             nome: "Enem",
             descricao: "Fique por dentro das novidades sobre o ENEM!",
             exemplo: "!Enem"
+        },
+        {
+            nome: "ImgFigurinha",
+            descricao: "Transforma Imagem em Figurinha!",
+            exemplo: "Envie uma foto ou marque uma imagem do chat com o comando !ImgFigurinha"
+        },
+        {
+            nome: "VidFigurinha",
+            descricao: "Transforma Vídeo/Gif em Figurinha Animada!",
+            exemplo: "Envie um vídeo/gif ou marque um vídeo/gif  do chat com o comando !VidFigurinha"
+        },
+        {
+            nome: "Meme",
+            descricao: "É enviado um Meme Aleatório!",
+            exemplo: "!Meme"
+        },
+        {
+            nome: "Fundador",
+            descricao: "Mostra o criador do Grupo!",
+            exemplo: "!Fundador"
+        },
+        {
+            nome: "Kick",
+            descricao: "Remove um integrante do grupo(O bot precisa ser admin)!",
+            exemplo: "!kick +55123456789"
+        },
+        {
+            nome: "Add",
+            descricao: "Adiciona um integrante do grupo(O bot precisa ser admin)!",
+            exemplo: "!Add +55123456789"
         }
     ],
     helpComandos: "",
 
     "AJUDA": async(client,mensagem)=>{
-
         await client.sendText(mensagem.from, `*LISTA DE COMANDOS*\n${palavrasReservadas.helpComandos}`);
     },
 
@@ -25,7 +54,7 @@ const palavrasReservadas = {
 
     "ENEM": async(client,mensagem)=>{
         await client.sendText(mensagem.from, "*O participante deve ingressar no local de prova entre 11h30 e 12h59. O portão fecha às 13h e o edital é claro: nenhuma pessoa pode entrar após este horário. Todos os participantes devem se dirigir para as suas salas de aplicação designadas no Cartão de Confirmação do Enem. A prova será iniciada às 13h30.*")
-    }
+    },
 
 }
 
