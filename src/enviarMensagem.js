07741f3c117515ca00e6211e48ced1174d4184fc
herik-silva <herik.silva.019@gmail.com>
2021-01-17 13:54:10 -0300

    Teste

--- src/enviarMensagem.js
+++ src/enviarMensagem.js
@@ -1,9 +1,16 @@
+const wa = require('@open-wa/wa-automate');
 const palavrasReservadas = require('./palavrasReservadas');
 
 // Envia a mensagem de acordo com a palavra reservada
+/**
+ * 
+ * @param {wa.Client} client 
+ */
 module.exports = function enviarMensagem(client){
+    client.group
     client.onMessage(async mensagem =>{
-        
+        console.log(client.getGroupInfo(mensagem.chatId).then((value)=>{console.log(value)}));
+        console.log(typeof mensagem);
         const palavraReservada = comando.split(palavrasReservadas.prefixo)[1].toUpperCase();
 
         const prefixoCorreto = palavrasReservadas.prefixo == comando.charAt(0);
