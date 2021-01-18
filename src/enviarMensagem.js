13ef8ae5053d2217c10a5ffab8e78b9627f906d0
MarcoAs25 <marcoas2566@gmail.com>
2021-01-18 02:09:23 -0300

    implementado método de imgFigurinha

--- src/enviarMensagem.js
+++ src/enviarMensagem.js
@@ -2,31 +2,61 @@ const palavrasReservadas = require('./palavrasReservadas');
 
 module.exports = function enviarMensagem(client){
     client.onMessage(async mensagem =>{
-        const prefixoCorreto = palavrasReservadas.prefixo == mensagem.body.charAt(0);
+        if(mensagem.type != "image"){
+            console.log(mensagem.type);
+            const prefixoCorreto = palavrasReservadas.prefixo == mensagem.body.charAt(0);
+            if(prefixoCorreto){
+                const comando = mensagem.body.split(" ");
+                // comando[0] -> !comando | comando[1...N] -> parametro
 
-        if(prefixoCorreto){
-            const comando = mensagem.body.split(" ");
-            // comando[0] -> !comando | comando[1...N] -> parametro
+                const palavraReservada = comando[0].split(palavrasReservadas.prefixo)[1].toUpperCase();
+                
+                const comandoExiste = palavrasReservadas[palavraReservada] != undefined;
+                
+                if(comandoExiste){
+                    var parametros = null;
 
-            const palavraReservada = comando[0].split(palavrasReservadas.prefixo)[1].toUpperCase();
-            
-            const comandoExiste = palavrasReservadas[palavraReservada] != undefined;
-            
-            if(comandoExiste){
-                var parametros = null;
-
-                // Verifica se no comando existe parâmetros.
-                if(comando.length > 1){
-                    parametros = [];
-                    for(let i=1; i<comando.length; i++){
-                        parametros.push(comando[i]);
+                    // Verifica se no comando existe parâmetros.
+                    if(comando.length > 1){
+                        parametros = [];
+                        for(let i=1; i<comando.length; i++){
+                            parametros.push(comando[i]);
+                        }
                     }
-                }
 
-                palavrasReservadas[palavraReservada](client,mensagem,parametros);
+                    palavrasReservadas[palavraReservada](client,mensagem,parametros);
+                }
+                else{
+                    await client.sendText(mensagem.from, "Comando inválido.\nPara visualizar os comandos existentes digite *!ajuda*");
+                }
             }
-            else{
-                await client.sendText(mensagem.from, "Comando inválido.\nPara visualizar os comandos existentes digite *!ajuda*");
+        }else{
+            console.log(mensagem.type);
+            const prefixoCorreto = palavrasReservadas.prefixo == mensagem.caption.charAt(0);
+            if(prefixoCorreto){
+                const comando = mensagem.caption.split(" ");
+                // comando[0] -> !comando | comando[1...N] -> parametro
+
+                const palavraReservada = comando[0].split(palavrasReservadas.prefixo)[1].toUpperCase();
+                
+                const comandoExiste = palavrasReservadas[palavraReservada] != undefined;
+                
+                if(comandoExiste){
+                    var parametros = null;
+
+                    // Verifica se no comando existe parâmetros.
+                    if(comando.length > 1){
+                        parametros = [];
+                        for(let i=1; i<comando.length; i++){
+                            parametros.push(comando[i]);
+                        }
+                    }
+                    console.log(palavrasReservadas[palavraReservada]);
+                    palavrasReservadas[palavraReservada](client,mensagem,parametros);
+                }
+                else{
+                    await client.sendText(mensagem.from, "Comando inválido.\nPara visualizar os comandos existentes digite *!ajuda*");
+                }
             }
         }
     });
