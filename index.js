0977c2e7058b21ad9498f0fbb2b1321de19182b0
herik-silva <herik.silva.019@gmail.com>
2021-02-07 12:36:52 -0300

    Server adicionado

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const robo = {
     enviarMensagem: require('./src/enviarMensagem'),
 }
 
+const app = require('express')()
+const http = require('http').createServer(app);
+
+http.listen(process.env.PORT, ()=>{
+    console.log("Rodando...");
+})
+
 const path = `${__dirname}/node_modules/puppeteer/.local-chromium/linux-818858/chrome-linux/chrome`
 
-wa.create({headless: true, executablePath: path,chromiumArgs: ['--no-sandbox']}).then(client => robo.enviarMensagem(client));
+wa.create({headless: true, executablePath: path,chromiumArgs: ['--no-sandbox','--ignore-google-port-numbers']}).then(client => robo.enviarMensagem(client));
