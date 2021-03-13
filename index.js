9d9c71cd37cd11e80c74ccb386cca016655f5045
herik-silva <herik.silva.019@gmail.com>
2021-03-13 17:46:13 -0300

    Configurado o browser

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const robo = {
     enviarMensagem: require('./src/enviarMensagem'),
 }
 
-wa.create({chromiumArgs: ['--no-sandbox','--ignore-google-port-numbers']}).then(client => robo.enviarMensagem(client)).catch(err => console.log(err));
+const path = `${__dirname}/node_modules/puppeteer/.local-chromium/linux-818858/chrome-linux/chrome`
+
+wa.create({headless: true, executablePath: path,chromiumArgs: ['--no-sandbox','--ignore-google-port-numbers']}).then(client => robo.enviarMensagem(client));
