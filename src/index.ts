96e00f084b6172ea4fe81f1141927b8352348c2e
herik-silva <herik.silva.019@gmail.com>
2021-03-17 19:49:06 -0300

    Configurado para rodar no heroku

--- src/index.ts
+++ src/index.ts
@@ -53,5 +53,6 @@ function ping(){
 
 ping();
 
+const path = `${__dirname}/../node_modules/puppeteer/.local-chromium/linux-818858/chrome-linux/chrome`;
 
-wa.create({headless: false,chromiumArgs: ['--no-sandbox','--ignore-google-port-numbers']}).then(client => robo.ouvirMensagens(client));
+wa.create({headless: true,executablePath: path,chromiumArgs: ['--no-sandbox','--ignore-google-port-numbers']}).then(client => robo.ouvirMensagens(client));
